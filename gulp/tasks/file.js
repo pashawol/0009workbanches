137cdc67690b103cef3aa4e491ce7e5d5cc304df
Pavel <wol1414@gmail.com>
2020-02-07 22:59:14 +0300

    form done

--- gulp/tasks/file.js
+++ gulp/tasks/file.js
@@ -1,9 +1,13 @@
-module.exports = function (){
+module.exports = function () {
+	const path = 'public';
 
-	
 	$.gulp.task('file', function () {
- 
-		return 	$.gulp.src([$.sourse + '/libs/**']).pipe($.gulp.dest($.public + '/libs')).on('end',$.browserSync.reload);
-});
 
+		// return 	$.gulp.src(['sourse/libs/**']).pipe($.gulp.dest('public/libs')).on('end',$.browserSync.reload);
+		return $.gulp.src('public/thanks.html')
+			// .pipe($.gp.rename("public/thanks.php"))
+			.pipe($.gp.rename(function (path) { path.extname = path.extname.replace('html', 'php') }))
+			.pipe($.gulp.dest(path))
+		//  .on('end',$.browserSync.reload);
+	});
 }
