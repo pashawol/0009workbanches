6d460af79c03fc80cd0d24a9f191f078e3b8eef9
Pavel <wol1414@gmail.com>
2020-04-21 22:47:45 +0300

    add price

--- gulp/tasks/file.js
+++ gulp/tasks/file.js
@@ -7,7 +7,7 @@ module.exports = function () {
 		return ($.gulp.src('public/thanks.html')
 			// .pipe($.gp.rename("public/thanks.php"))
 			.pipe($.gp.rename(function (path) { path.extname = path.extname.replace('html', 'php') }))
-			.pipe($.gulp.dest(path), $.del([$.public + '/thanks.html'], { force: true }))
+			// .pipe($.gulp.dest(path), $.del([$.public + '/thanks.html'], { force: true }))
 
 		)
 		//  .on('end',$.browserSync.reload);
